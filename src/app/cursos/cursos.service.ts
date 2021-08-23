e189618c68961fca66b445ab58afa1081479762b
Gthalles <thalles@gempe.dev>
2021-08-23 16:27:37 -0300

    Escopo de instâncias de serviços e organização de módulos utilizando os Services

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -3,8 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class CursosService {
+  private cursos: string[] = ["Angular", "Docker", "SpringBoot"];
+
+  constructor() {
+    console.log('CursosService');
+  }
+
   getCursos() {
-    return ["Angular", "Docker", "SpringBoot"];
+    return this.cursos;
+  }
+
+  addCursos(curso: string) {
+    this.cursos.push(curso);
   }
 
 }
